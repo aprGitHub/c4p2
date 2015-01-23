5ab6d61b8740ec923a43edb16ad1b19d845518cc
aprGitHub <apr.workdir@gmail.com>
2015-01-23 19:33:11 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,33 +1,30 @@
+################################
+# QUESTION 2:Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
+# Use the base plotting system to make a plot answering this question.
+# For deeper explanation turn to: https://github.com/aprGitHub/c4p2
+################################
+
 library(plyr)
-source("downloadFromURLAndUnzip.R")
+###################################
+# MAIN ROUTINE: plot2.R
+# 1. Ad-hoc function to download the data for this exercise, be patient, it takes a while:-! 
+source("downloadAndUnzip.R")
+data <- downloadAndUnzip()
 
-# 1. Ad-hoc function to download the data for this exercise 
-downloadFromURLAndUnzip()
+# 2. Get the data associated to Baltimore (i.e. fips == "24510")
+dataBaltimore <- subset(data, fips == "24510", select=c(year, Emissions)) 
+rm(data) 
 
-# 2. Read the data into R objects. This first line will likely take a few seconds. Be patient!
-NEI <- readRDS("./data/summarySCC_PM25.rds")
-SCC <- readRDS("./data/Source_Classification_Code.rds")
-
-# 3. Merge the data add the Short.Name related to the SCC code
-SCCsub <- subset(SCC,select=c(SCC,Short.Name))
-NEIsub <- subset(NEI,select=-c(Pollutant))# remove Pollutant because it is redundant: unique(data$Pollutant)="PM25-PRI" allways
-rm(NEI,SCC) # Remove the object (otherwise my old laptop may run out of memory:)
-
-data <- merge(SCCsub, NEIsub,by="SCC") 
-rm(NEIsub,NEIsub) # Remove the object (otherwise my old laptop may run out of memory:)
-
-# 4. Get the data associated to Baltimore (i.e. fips == "24510")
-dataBaltimore <- subset(data, fips == "24510",select=c(year, Emissions)) # 
-rm(data)
-
-# 5. Get the sum of the emissions by year
+# 3. Get the sum of the emissions by year
 dataSumEmissionsByYear <- ddply(dataBaltimore, .(year), summarise, sumEmissions = sum(Emissions, na.rm = TRUE))
 
-# 6. Plot and save the file
+# 4. Plot and save the file
 plot(dataSumEmissionsByYear$year,dataSumEmissionsByYear$sumEmissions,type="b",xlab="Year",ylab="PM25-PRI Emissions",main="Emissions by Year in Baltimore City")
-#Plot the x-axis that contains the year
-axis(1, at=as.integer(aggData$Year), las=1)
+# axis 
 
 pngFile <- "plot2.png"
 dev.copy(png, file = pngFile,  bg = "white")
 dev.off()
+###################################
+
+
